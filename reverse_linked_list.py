fecace269414a9fbd9fbb83c56bd6ce992362e48
Sultan477 <Aktsultan@mail.ru>
2024-07-27 16:37:27 +0300

    206. Reverse Linked List

---
+++ reverse_linked_list.py
@@ -0,0 +1,20 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+
+        if head is None or head.next is None:
+            return head
+
+        prev, curr, tmp = None, head, None
+        while curr:
+            tmp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = tmp
+
+        return prev
